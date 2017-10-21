275178851a100f0f3b8410a944b0b501f220f7d7
Kevin MIller <kevin@driftabout.com>
2017-10-20 17:00:06 -0700

    finsihed problem 5

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ function sumArray(testArray){ //eslint-disable-line
 }
 
 // DONE: Here is the test for sumArray(); uncomment it to run it
-testSumArray(testArray);
+//testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -105,10 +105,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(testArray){ //eslint-disable-line
-
+  var arrProduct = 1;
+  for(var i = 0; i < testArray.length; i++){
+    arrProduct = multiply(testArray[i], arrProduct)[0];
+  }
+  var message = [arrProduct, 'The numbers ' + testArray.join(',') + ' have a product of ' + arrProduct + '.' ];
+  return message;
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(2,3,4);
+testMultiplyArray(2,3,4);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
